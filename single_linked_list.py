dd8d4b40292fb939a9de5ae0229c72df4c688b5e
unknown <shubhamrocks888@gmail.com>
2020-05-21 15:42:23 +0530

    regular command file added

--- single_linked_list.py
+++ single_linked_list.py
@@ -5,110 +5,61 @@ class node:
 
 class linked_list:
     def __init__(self):
-        self.head = node()
+        self.head = None
 
-##TO APPEND A NODE
+##TO Add A NODE AT THE  START
 
+    def push(self,data):
+        new_node = node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+##    Time complexity of push() is O(1) as it does constant amount of work.
+
+##ADD A NODE AFTER A GIVEN NODE
+
+    def insertafter(self,prev_node,data):
+        if prev_node is None:
+            print ("the given previous node must inn linked list")
+        new_node = node(data)
+
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+        
+##Time complexity of insertAfter() is O(1) as it does constant amount of work.
+
+##ADD A NODE AT THE END
+        
     def append(self,data):
-        new_node = node(data)
-        cur = self.head
-        while cur.next:
-            cur = cur.next
-        cur.next = new_node
-
-# TO FIND THE LENGTH OF LINKED LIST
-
-    def length(self):
-        cur = self.head
-        total = 0
-        while cur.next:
-            total+=1
-            cur = cur.next
-        return total
-
-# DISPLAY ALL THE ELEMENTS OF A LIST
-
-    def display(self):
-        elem=[]
-        cur = self.head
-        while cur.next:
-            cur = cur.next
-            elem.append(cur.data)
-        return elem
-    
-# TO GET THE ELEMENTS AT A CERTAIN INDEX
-
-    def get(self,index):
-        if index >= my_list.length():
-            return "index out of range"
+        new_node = node(data) 
         
-##        cur = self.head
-##        cur_idx = -1
-##        while cur.next:
-##            cur_idx+=1
-##            cur=cur.next
-##            if cur_idx==index:
-##                return cur.data
-        cur_idx = 0
+
+        if self.head is None:
+            self.head = new_node
+            return
+
         cur_node = self.head
-
-        while True:
+        while cur_node.next:
             cur_node = cur_node.next
-            if index==cur_idx:
-                return cur_node.data
-            cur_idx+=1
+        cur_node.next = new_node
 
-##TO ERASE A NODE FROM CERTAIN INDEX
-
-    def erase(self,index):
-        if index >= my_list.length():
-            return "index out of range"
-
-        cur_idx = 0
-        cur_node = self.head
-
-        while True:
-            last_node = cur_node
-            cur_node = cur_node.next
-
-            if index==cur_idx:
-                last_node.next = cur_node.next
-                return "node has been deleted"
-            cur_idx+=1            
-
-##ADD A NODE AT CERTAIN INDEX
-
-    def add(self,index,data):
-        if index >= my_list.length():
-            return "index out of range"
-
-        new_node = node(data)
-        cur_idx = 0
-        cur_node = self.head
-
-        while True:
-            last_node = cur_node
-            cur_node = cur_node.next
-
-            if index==cur_idx:
-                last_node.next = new_node
-                new_node.next = cur_node
-                return "node has been added"
-            cur_idx+=1
+##Time complexity of append is O(n) where n is the number of nodes in linked list. Since there is a loop from head to end, the function does O(n) work.
+##This method can also be optimized to work in O(1) by keeping an extra pointer to tail of linked list/
             
-        
-my_list = linked_list()
-print (my_list.display())
+    def printlist(self):
+        cur_node = self.head
+        while cur_node:
+            print (cur_node.data,end=" ")
+            cur_node = cur_node.next
 
-my_list.append(1)
-my_list.append(2)
-my_list.append(3)
-
-print (my_list.display())
-my_list.add(2,5)
-print (my_list.display())
-##print (my_list.erase(2))
-##print (my_list.display())
+l = linked_list()
+l.append(6)
+l.push(7)
+l.push(5)
+l.append(8)
+l.insertafter(l.head.next,9) 
+  
+l.printlist()
 
 
 
